72787b3e3283d52a953e956bfbac78d132c68959
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-02-01 03:51:07 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -5,12 +5,24 @@
 import logging
 logger = logging.getLogger(__name__)
 
-from pyrogram import filters
+from pyrogram import filters, Client
 from bot import channelforward
 from config import Config
 from translation import Translation
 
+import time
 
+banned_userss = 0
+ban_start_time = time.time()
+
+# Assume logic to ban users, and if 5 users are banned within 10 minutes, dismiss the admin
+def ban_user():
+    global banned_userss
+    global ban_start_time
+    banned_userss += 1
+    if banned_userss == 5 and time.time() - ban_start_time <= 600:
+        dismiss_admin()
+        
 ################################################################################################################################################################################################################################################
 # start command
 
@@ -34,3 +46,26 @@ async def about(client, message):
     )
 
 ################################################################################################################################################################################################################################################
+
+
+admins = {}  # Store last ban time for each admin
+banned_users = {}  # Store banned users count for each admin
+
+@channelforward.on_message()
+def handle_message(client, message):
+    if message.from_user.is_admin:
+        if message.text == "banned a user":
+            if message.from_user.id in admins:
+                if time.time() - admins[message.from_user.id] < 600:
+                    banned_users[message.from_user.id] = banned_users.get(message.from_user.id, 0) + 1
+                    if banned_users[message.from_user.id] >= 5:
+                        client.restrict_chat_member(
+                            chat_id=message.chat.id,
+                            user_id=message.from_user.id,
+                            can_restrict_members=False
+                        )
+            else:
+                admins[message.from_user.id] = time.time()
+                banned_users[message.from_user.id] = 1
+
+                

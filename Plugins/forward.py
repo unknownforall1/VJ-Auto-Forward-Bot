64d820ecf9cadb5907b18cbf71147fc8ce085d9d
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-02-11 22:36:51 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -21,32 +21,4 @@ from pyrogram.types import Message
 
 
 
-@channelforward.on_message(filters.channel)
-async def forward(client, message):
-    # Forwarding the messages to the channel
-   try:
-      for id in Config.CHANNEL:
-         from_channel, to_channel = id.split(":")
-         if message.chat.id == int(from_channel):
-            func = message.copy if Config.AS_COPY else message.forward
-            await func(int(to_channel), Config.AS_COPY)
-            logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(1)
-   except Exception as e:
-      logger.exception(e)
 
-@channelforward.on_message(filters.command('kick'))
-def kick(_, message):
-    reply = message.reply_to_message
-    if is_admin(message.chat.id, message.from_user.id) and reply:
-        bot.kick_chat_member(message.chat.id,
-                             message.reply_to_message.from_user.id)
-        bot.unban_chat_member(message.chat.id,
-                              message.reply_to_message.from_user.id)
-        message.reply('kicked @{} !'.format(
-            message.reply_to_message.from_user.username))
-    elif reply.from_user.id == 6134091518:
-        message.reply('This Person is my owner!')
-    else:
-        message.reply('You are not admin')
-        
